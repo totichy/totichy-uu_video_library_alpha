ddb57ab2d360cfaf4f9904b5bf2f470fb2160594
spatrickeu <83925040+spatrickeu@users.noreply.github.com>
2021-05-12 11:22:30 +0200

    Delete .eslintrc.js

--- uu_videolibrary_maing01-hi/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-let config = {
-  extends: ["./" + path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
-};
-
-module.exports = config;
