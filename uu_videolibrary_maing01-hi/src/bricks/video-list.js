448c1d2e0308a88eed0d9cd2f679be6cc9b5eb89
spatrickeu <83925040+spatrickeu@users.noreply.github.com>
2021-05-12 14:40:40 +0200

    Update video-list.js

--- uu_videolibrary_maing01-hi/src/bricks/video-list.js
+++ uu_videolibrary_maing01-hi/src/bricks/video-list.js
@@ -1,5 +1,7 @@
 //@@viewOn:imports
 import UU5 from "uu5g04";
+import "uu5g04-block-layout";
+import Uu5Tiles from "uu5tilesg02";
 import { createVisualComponent, useLsi } from "uu5g04-hooks";
 import Config from "./config/config";
 import Video from "./video";
@@ -12,6 +14,36 @@ const STATICS = {
   nestingLevel: "bigBoxCollection",
   //@@viewOff:statics
 };
+const FILTERS = [
+  {
+    key: "kategorie",
+    label: { cs: "Kategorie", en: "Kategory" },
+    filterFn: (item, value) => {
+      let fragments = value.split(/[\s,.-;:_]/);
+      return fragments.some(frag => {
+        let itemValue =
+          typeof item.category === "object"
+            ? UU5.Common.Tools.getLsiItemByLanguage(item.category)
+            : item.category;
+        return itemValue.toLowerCase().indexOf(frag.toLowerCase()) !== -1;
+      });
+    }
+  },
+  {
+    key: "Názvu",
+    label: { cs: "Název", en: "Title" },
+    filterFn: (item, value) => {
+      let fragments = value.split(/[\s,.-;:_]/);
+      return fragments.some(frag => {
+        let itemValue =
+          typeof item.title === "object"
+            ? UU5.Common.Tools.getLsiItemByLanguage(item.title)
+            : item.title;
+        return itemValue.toLowerCase().indexOf(frag.toLowerCase()) !== -1;
+      });
+    }
+  }
+];
 
 export const VideoList = createVisualComponent({
   ...STATICS,
@@ -48,29 +80,35 @@ export const VideoList = createVisualComponent({
       return (
         <div>
           <UU5.Bricks.Container>
-            <UU5.Bricks.Header level="1" content={VideoListHeader} underline={true} />
+            <UU5.Bricks.Header level={1} content={VideoListHeader} underline={true} />
             <UU5.Common.Error content={noVideoCgi} />
           </UU5.Bricks.Container>
         </div>
       );
     }
     return (
-      <div>
+      
+      <div> 
+        <Uu5Tiles.ControllerProvider data={videos} filters={FILTERS}>
+  <Uu5Tiles.FilterBar />
+  </Uu5Tiles.ControllerProvider>
         <UU5.Bricks.Container>
-          <UU5.Bricks.Header level="1" content={VideoListHeader} underline={true} />
-          {videos.map((video) => {
-            return (
-              // eslint-disable-next-line react/jsx-key
+          <UU5.Bricks.Header level={1} content={VideoListHeader} underline={true} />
+            {videos.map((video) => {
+            return (  
               <UU5.Bricks.Div>
-                <Video key={video.code} video={video} onDelete={onDelete} />
+                <Video video={video} onDelete={onDelete} />
               </UU5.Bricks.Div>
             );
           })}
+
         </UU5.Bricks.Container>
       </div>
     );
     //@@viewOff:render
+    
   },
 });
+
 
 export default VideoList;
