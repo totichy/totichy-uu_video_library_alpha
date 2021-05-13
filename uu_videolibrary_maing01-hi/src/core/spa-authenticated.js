240148049b75930226348d2d6075420f29515b50
spatrickeu <83925040+spatrickeu@users.noreply.github.com>
2021-05-14 01:09:12 +0200

    New layout

--- uu_videolibrary_maing01-hi/src/core/spa-authenticated.js
+++ uu_videolibrary_maing01-hi/src/core/spa-authenticated.js
@@ -4,7 +4,7 @@ import "uu5g04-bricks";
 import { createVisualComponent, useState } from "uu5g04-hooks";
 import Plus4U5 from "uu_plus4u5g01";
 import "uu_plus4u5g01-app";
-
+import Bottom from "./bottom";
 import Config from "./config/config";
 import Left from "./left";
 //@@import Bottom from "./bottom";
@@ -57,10 +57,11 @@ export const SpaAuthenticated = createVisualComponent({
       <Plus4U5.App.MenuProvider activeItemId={initialActiveItemId}>
         <Plus4U5.App.Page
           {...props}
+          top={<Plus4U5.App.TopBt />}
           topFixed="smart"
+          bottom={<Bottom />}
           leftWrapperProps={{ style: { backgroundColor: '#fafafa' } }}
-          top={<Plus4U5.App.Top content="TEAM 17" />}
-          type={1}
+          type={3}
           displayedLanguages={["cs", "en"]}
           left={<Left />}
           leftWidth="!xs-300px !s-300px !m-288px !l-288px !xl-288px"
@@ -69,6 +70,7 @@ export const SpaAuthenticated = createVisualComponent({
           leftResizableMinWidth={220}
           leftResizableMaxWidth={500}
           isLeftOpen="m l xl"
+          showLeftToggleButton
           fullPage
         >
           <Plus4U5.App.MenuConsumer>
