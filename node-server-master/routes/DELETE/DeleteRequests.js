9d5e2b0a13934299816b97c6a601a930199e837e
spatrickeu <83925040+spatrickeu@users.noreply.github.com>
2021-05-25 03:13:05 +0200

    Update DeleteRequests.js

--- node-server-master/routes/DELETE/DeleteRequests.js
+++ node-server-master/routes/DELETE/DeleteRequests.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.json());
 
 //DELETE a video
-router.delete('/delete/:code', async (req, res) => {
-    uuVideo.findOneAndDelete({ code: req.params.code }, function (err, delVideo) {
+router.delete('/delete', async (req, res) => {
+    uuVideo.findOneAndDelete({ code: req.body.code }, function (err, delVideo) {
         if (err) {
             return res.status(400).json(err);
         } else {
-            return res.status(200).json({ delete_message: `video with id: ${req.params.code} was sucessfully removed from database` })
+            return res.status(200).json({ delete_message: `video with id: ${req.body.code} was sucessfully removed from database` })
 
         }
     })
