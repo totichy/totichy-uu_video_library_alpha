985824ecbd57d99e4ee2064c285e7c5880bed793
spatrickeu <83925040+spatrickeu@users.noreply.github.com>
2021-05-25 19:54:18 +0200

    Oprava filtrování podle kategorií

--- uu_videolibrary_maing01-hi/src/bricks/video-list.js
+++ uu_videolibrary_maing01-hi/src/bricks/video-list.js
@@ -1,11 +1,10 @@
 //@@viewOn:imports
 import UU5 from "uu5g04";
 import "uu5g04-block-layout";
-import { createVisualComponent, useLsi, useDataList } from "uu5g04-hooks";
+import { createVisualComponent, useLsi } from "uu5g04-hooks";
 import Config from "./config/config";
 import Video from "./video";
 import VideoLsi from "../config/video";
-import Calls from "../calls";
 //@@viewOff:imports
 
 const STATICS = {
@@ -14,12 +13,6 @@ const STATICS = {
   nestingLevel: "bigBoxCollection",
   //@@viewOff:statics
 };
-
-function categorySelection(queryString) {
-  let urlParams = new URLSearchParams(queryString);
-  return urlParams.get("category");
-}
-
 
 export const VideoList = createVisualComponent({
   ...STATICS,
@@ -42,91 +35,38 @@ export const VideoList = createVisualComponent({
   },
   //@@viewOff:defaultProps
 
-  
-
   render({ videos, onDelete, onRating, onUpdate }) {
-
-    const categoryListResult = useDataList({
-      handlerMap: {
-        load: Calls.listCategory,
-      },
-      initialDtoIn: { data: {} },
-    });
-
-    const categoryMap = {};
-    if (categoryListResult.data) {
-      categoryListResult.data.forEach((category) => (categoryMap[category.data.categoryId] = category.data.categoryName) );
-    }
-     const dada = categoryMap[categorySelection(window.location.search)];
-
-
-
     //console.log(dadacategoryId);
     //@@viewOn:private
     const noVideo = VideoLsi.noVideo || {};
 
-    const AllVideos = VideoLsi.AllVideos || {};
-    const SelectedVideos = VideoLsi.SelectedVideos || {};
-
     let noVideoCgi = useLsi(noVideo);
-    let VideoListHeader = useLsi(AllVideos);
-    let SelectedVideoListHeader = useLsi(SelectedVideos);
+
     let countVideo = 0;
     //@@viewOff:private
 
     //@@viewOn:interface
     //@@viewOff:interface
-    let VideoHeader = "";
 
-    if (categorySelection(window.location.search) === null) {
-      VideoHeader = VideoListHeader;
-    } else {
-      VideoHeader = SelectedVideoListHeader + ": " + dada;
-    }
     //@@viewOn:render
     if (videos.length === 0) {
       return (
-        <div>
-          <UU5.Bricks.Container>
-            <UU5.Bricks.Header level={3} content={VideoHeader} underline={true} />
-            <UU5.Common.Error content={noVideoCgi} />
-          </UU5.Bricks.Container>
-        </div>
+        <>
+          <UU5.Bricks.Header level={3} content={VideoHeader} underline={true} />
+          <UU5.Common.Error content={noVideoCgi} />
+        </>
       );
     }
     return (
-      <div>
-        <UU5.Bricks.Container>
-          <UU5.Bricks.Header level={3} content={VideoHeader} underline={true} />
-
-          {videos.map((video, index) => {
-            if (
-              video.data.category.find((element) => element == categorySelection(window.location.search)) ===
-              categorySelection(window.location.search)
-            ) {
-              countVideo++;
-              return (
-                <UU5.Bricks.Div key={index}>
-                  <Video video={video.data} onDelete={onDelete} onUpdate={onUpdate} onRating={onRating} />
-                </UU5.Bricks.Div>
-              );
-            } else if (categorySelection(window.location.search) === null) {
-              countVideo = 1;
-              return (
-                <UU5.Bricks.Div key={index}>
-                  <Video video={video.data} onDelete={onDelete} onUpdate={onUpdate} onRating={onRating} />
-                </UU5.Bricks.Div>        
-              );
-            } else if (countVideo === 0 && index === videos.length - 1) {
-              return (
-                <div>
-                  <UU5.Common.Error content={noVideoCgi} />
-                </div>
-              );
-            }
-          })}
-        </UU5.Bricks.Container>
-      </div>
+      <>
+        {videos.map((video, index) => {
+          return (
+            <UU5.Bricks.Div key={index}>
+              <Video video={video.data} onDelete={onDelete} onUpdate={onUpdate} onRating={onRating} />
+            </UU5.Bricks.Div>
+          );
+        })}
+      </>
     );
     //@@viewOff:render
   },
--- uu_videolibrary_maing01-hi/src/routes/home.js
+++ uu_videolibrary_maing01-hi/src/routes/home.js
@@ -1,13 +1,15 @@
 //@@viewOn:imports
 import "uu5g04-bricks";
-import { createVisualComponent, useRef, useLsi } from "uu5g04-hooks";
+import { createVisualComponent, useRef, useLsi, useDataList } from "uu5g04-hooks";
 import "uu_plus4u5g01-bricks";
 import UU5 from "uu5g04";
+import Uu5Tiles from "uu5tilesg02";
 import Config from "./config/config.js";
 import VideoList from "../bricks/video-list.js";
 import VideoProvider from "../bricks/video-provider.js";
 import VideoCreate from "../bricks/video-create.js";
 import VideoLsi from "../config/video";
+import Calls from "../calls";
 //@@viewOff:imports
 
 const STATICS = {
@@ -15,6 +17,11 @@ const STATICS = {
   displayName: Config.TAG + "Home",
   //@@viewOff:statics
 };
+
+function categorySelection(queryString) {
+  let urlParams = new URLSearchParams(queryString);
+  return urlParams.get("category");
+}
 
 export const Home = createVisualComponent({
   ...STATICS,
@@ -41,34 +48,53 @@ export const Home = createVisualComponent({
     const wasCreated = VideoLsi.wasCreated || {};
     const createError = VideoLsi.errorCreate || {};
     const errorServerData = VideoLsi.errorServer || {};
+    const AllVideos = VideoLsi.AllVideos || {};
+    const SelectedVideos = VideoLsi.SelectedVideos || {};
 
+    let VideoListHeader = useLsi(AllVideos);
+    let SelectedVideoListHeader = useLsi(SelectedVideos);
     let videoWithTitle = useLsi(delVideoText);
     let wasDeletedC = useLsi(wasDeleted);
     let wasCreatedC = useLsi(wasCreated);
     let errorCreated = useLsi(createError);
     let serverErrorData = useLsi(errorServerData);
 
+    const categoryListResult = useDataList({
+      handlerMap: {
+        load: Calls.listCategory,
+      },
+      initialDtoIn: { data: {} },
+    });
+
+    const categoryMap = {};
+    if (categoryListResult.data) {
+      categoryListResult.data.forEach(
+        (category) => (categoryMap[category.data.categoryId] = category.data.categoryName)
+      );
+    }
+    const dada = categoryMap[categorySelection(window.location.search)];
+
     //@@viewOn:private
     function showError(content) {
       UU5.Environment.getPage().getAlertBus().addAlert({
-    content,
-    colorSchema: "red",
-    closeTimer: 3000
-      })
+        content,
+        colorSchema: "red",
+        closeTimer: 3000,
+      });
     }
 
     function showSuccess(content) {
       UU5.Environment.getPage().getAlertBus().addAlert({
-    content,
-    colorSchema: "green",
-    closeTimer: 3000
-      })
+        content,
+        colorSchema: "green",
+        closeTimer: 3000,
+      });
     }
 
     async function handleCreateVideo(video) {
       try {
-      await createVideoRef.current(video);
-      showSuccess(`${videoWithTitle} ${video.title} ${wasCreatedC}`);
+        await createVideoRef.current(video);
+        showSuccess(`${videoWithTitle} ${video.title} ${wasCreatedC}`);
       } catch (e) {
         if (e.response) {
           // client received an error response (5xx, 4xx)
@@ -80,12 +106,12 @@ export const Home = createVisualComponent({
           showError(errorCreated);
         }
       }
-    }    
+    }
 
     async function handleUpdateVideo(video) {
       try {
-      await updateVideoRef.current(video);
-      showSuccess(`${videoWithTitle} ${video.title} ${wasCreatedC}`);
+        await updateVideoRef.current(video);
+        showSuccess(`${videoWithTitle} ${video.title} ${wasCreatedC}`);
       } catch (e) {
         if (e.response) {
           // client received an error response (5xx, 4xx)
@@ -97,14 +123,13 @@ export const Home = createVisualComponent({
           showError(errorCreated);
         }
       }
-    }    
+    }
 
     async function handleDeleteVideo(video) {
       try {
-      await deleteVideoRef.current({code : video.code},video.code);
-      showSuccess(`${videoWithTitle} ${video.title} ${wasDeletedC}`);
+        await deleteVideoRef.current({ code: video.code }, video.code);
+        showSuccess(`${videoWithTitle} ${video.title} ${wasDeletedC}`);
       } catch (e) {
-
         if (e.response) {
           // client received an error response (5xx, 4xx)
           showError(`ERROR: ${e.response.data.error_message}`);
@@ -114,38 +139,69 @@ export const Home = createVisualComponent({
         } else {
           showError(`Deletion of ${video.title} is failed.`);
         }
-     
       }
-    }   
+    }
 
     async function handleRatingVideo(video, mrating) {
       try {
-      await ratingVideoRef.current({code : video.code, mrating: Number(mrating)});
-      showSuccess("Děkujeme za hodnocení, které bylo: " + mrating );
+        await ratingVideoRef.current({ code: video.code, mrating: Number(mrating) });
+        showSuccess("Děkujeme za hodnocení, které bylo: " + mrating);
       } catch (e) {
-      showError(`Rating of ${video.title} is failed.`);
+        showError(`Rating of ${video.title} is failed.`);
       }
-    }  
+    }
     function renderLoad() {
       return <UU5.Bricks.Loading />;
     }
 
     function renderError(errorData) {
       return <UU5.Bricks.Error content={serverErrorData} />;
-      }
+    }
+    let VideoHeader = "";
+
+    if (categorySelection(window.location.search) === null) {
+      VideoHeader = VideoListHeader;
+    } else {
+      VideoHeader = SelectedVideoListHeader + ": " + dada;
+    }
 
     function renderReady(videos) {
-       videos.sort((a, b) => (a.code < b.code) ? 1 : -1);
+      videos.sort((a, b) => (a.code < b.code ? 1 : -1));
+      console.log(videos);
+      var videox;
+      if (categorySelection(window.location.search) === null) {
+        videox = videos;
+      } else {
+        videox = videos.filter(function (hero) {
+          return (
+            hero.data.category.find((element) => element == categorySelection(window.location.search)) ===
+            categorySelection(window.location.search)
+          );
+        });
+        console.log(videox);
+      }
       return (
         <>
           <VideoCreate onCreate={handleCreateVideo} />
           <UU5.Bricks.Section>
-            <VideoList videos={videos} onDelete={handleDeleteVideo} onUpdate={handleUpdateVideo} onRating={handleRatingVideo} />
+            <div>
+              <UU5.Bricks.Container>
+                <UU5.Bricks.Header level={3} content={VideoHeader} underline={true} />
+                <Uu5Tiles.ControllerProvider data={videox}>
+                  <Uu5Tiles.InfoBar />
+                </Uu5Tiles.ControllerProvider>
+                <VideoList
+                  videos={videox}
+                  onDelete={handleDeleteVideo}
+                  onUpdate={handleUpdateVideo}
+                  onRating={handleRatingVideo}
+                />
+              </UU5.Bricks.Container>
+            </div>
           </UU5.Bricks.Section>
         </>
       );
     }
-
 
     //@@viewOn:interface
 
@@ -157,26 +213,24 @@ export const Home = createVisualComponent({
       <div>
         <VideoProvider>
           {({ state, data, newData, pendingData, errorData, handlerMap }) => {
-
             createVideoRef.current = handlerMap.createVideo;
             deleteVideoRef.current = handlerMap.deleteVideo;
             ratingVideoRef.current = handlerMap.ratingVideo;
             updateVideoRef.current = handlerMap.updateVideo;
 
             switch (state) {
-                case "pending":
-                case "pendingNoData":
-                  return renderLoad();
-                case "error":
-                case "errorNoData":
-                  return renderError(errorData);
-                case "itemPending":
-                case "ready":
-                case "readyNoData":
-                default:
-                  return renderReady(data);  
+              case "pending":
+              case "pendingNoData":
+                return renderLoad();
+              case "error":
+              case "errorNoData":
+                return renderError(errorData);
+              case "itemPending":
+              case "ready":
+              case "readyNoData":
+              default:
+                return renderReady(data);
             }
-
           }}
         </VideoProvider>
       </div>
